bea6ed3e8c836fbca36553a7a3eb2cccd1468712
dmytro <dmysamo@gmail.com>
2025-03-17 14:15:22 +0200

    add base settings

---
+++ backend/core/settings.py
@@ -0,0 +1,34 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    """Configuration settings for the application."""
+
+    environment: str
+
+    postgres_port: int
+    postgres_user: str
+    postgres_password: str
+    postgres_db: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
+    )
+
+    @property
+    def db_host(self) -> str:
+        """Return the database host based on the environment."""
+        if self.environment == "local":
+            return "localhost"
+        return "db"
+
+    @property
+    def database_url(self) -> str:
+        """Generate the database URL based on the environment."""
+        return (
+            f"postgresql+psycopg2://{self.postgres_user}:{self.postgres_password}"
+            f"@{self.db_host}:{self.postgres_port}/{self.postgres_name}"
+        )
+
+
+settings = Settings()
